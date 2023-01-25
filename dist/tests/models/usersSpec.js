e05c4069b4c14d3ca6224efdf2d393b185074a3a
Ali Maher Al-sheikh <aly.alsheck@gmail.com>
2023-01-24 23:59:39 -0800

    addiding tests for user model

---
+++ dist/tests/models/usersSpec.js
@@ -0,0 +1,75 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const user_1 = require("../../models/user");
+console.log('testing models environment' + process.env.ENVIRONMENT);
+const UserData = {
+    first_name: 'mero',
+    last_name: 'mondo',
+    date_of_creation: '12-13-1996',
+    email: 'mando@example.com',
+    user_name: 'meromando',
+    user_password: '123456abcd',
+    phone: '0111111111',
+    user_type: 'guest',
+};
+let user = {
+    id: 1,
+    first_name: 'mero',
+    last_name: 'mondo',
+    date_of_creation: '12-13-1996',
+    email: 'mando@example.com',
+    user_name: 'meromando',
+    user_password: '123456abcd',
+    phone: '0111111111',
+    user_type: 'guest',
+};
+describe('Tests for user model', () => {
+    it('expects the user to have  the same data in UserData', () => __awaiter(void 0, void 0, void 0, function* () {
+        user = yield user_1.UserModel.insert(UserData);
+        const response = Object.assign({}, user);
+        delete response.id;
+        expect(response).toEqual({
+            first_name: 'mero',
+            last_name: 'mondo',
+            date_of_creation: '12-13-1996',
+            email: 'mando@example.com',
+            user_name: 'meromando',
+            user_password: '123456abcd',
+            phone: '0111111111',
+            user_type: 'guest',
+        });
+    }));
+    it('expects user id to be number greater than 0', () => {
+        expect(user.id).toBeGreaterThan(0);
+    });
+    it('expect the response to be the same as user', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield user_1.UserModel.select(1);
+        expect(response).toEqual(user);
+    }));
+    it('expects the responce to be array containing user as last element', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield user_1.UserModel.selectAll();
+        expect(response[response.length - 1]).toEqual(user);
+    }));
+    it('expect the response to be the same as user', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield user_1.UserModel.selectUser(user.user_name);
+        expect(response).toEqual(user);
+    }));
+    it('expects to the response to be user after updating property' + ' email', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield user_1.UserModel.update(Object.assign(Object.assign({}, UserData), { email: 'mero2@eampample.com' }), user.id);
+        expect(response).toEqual(Object.assign(Object.assign({}, user), { email: 'mero2@eampample.com' }));
+    }));
+    it('expects to the response to be empty', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield user_1.UserModel.remove(user.id);
+        const response = yield user_1.UserModel.select(user.id);
+        expect(response).toBeFalsy();
+    }));
+});
--- src/tests/models/usersSpec.ts
+++ src/tests/models/usersSpec.ts
@@ -1,10 +1,81 @@
-import supertest from 'supertest';
-import { app } from '../server';
-console.log('testing');
-const AppRequest = supertest(app);
-describe('Test endpoint responses', () => {
-    it('gets the api endpoint', async () => {
-        const response = await AppRequest.get('/user');
-        expect(response.status).toBe(200);
+import { UserModel } from '../../models/user';
+import { User, UserDTO } from '../../types/types';
+console.log('testing models environment' + process.env.ENVIRONMENT);
+const UserData: UserDTO = {
+    first_name: 'mero',
+    last_name: 'mondo',
+    date_of_creation: '12-13-1996',
+    email: 'mando@example.com',
+    user_name: 'meromando',
+    user_password: '123456abcd',
+    phone: '0111111111',
+    user_type: 'guest',
+};
+let user: User = {
+    id: 1,
+    first_name: 'mero',
+    last_name: 'mondo',
+    date_of_creation: '12-13-1996',
+    email: 'mando@example.com',
+    user_name: 'meromando',
+    user_password: '123456abcd',
+    phone: '0111111111',
+    user_type: 'guest',
+};
+describe('Tests for user model', () => {
+    it('expects the user to have  the same data in UserData', async () => {
+        user = await UserModel.insert(UserData);
+        const response: {
+            id?: number;
+            first_name: string;
+            last_name: string;
+            date_of_creation: string;
+            email: string;
+            user_name: string;
+            user_password: string;
+            phone: string;
+            user_type: string;
+        } = { ...user };
+        delete response.id;
+        expect(response).toEqual({
+            first_name: 'mero',
+            last_name: 'mondo',
+            date_of_creation: '12-13-1996',
+            email: 'mando@example.com',
+            user_name: 'meromando',
+            user_password: '123456abcd',
+            phone: '0111111111',
+            user_type: 'guest',
+        });
+    });
+    it('expects user id to be number greater than 0', () => {
+        expect(user.id).toBeGreaterThan(0);
+    });
+    it('expect the response to be the same as user', async () => {
+        const response = await UserModel.select(1);
+        expect(response).toEqual(user);
+    });
+    it('expects the responce to be array containing user as last element', async () => {
+        const response = await UserModel.selectAll();
+        expect(response[response.length - 1]).toEqual(user);
+    });
+    it('expect the response to be the same as user', async () => {
+        const response = await UserModel.selectUser(user.user_name);
+        expect(response).toEqual(user);
+    });
+    it(
+        'expects to the response to be user after updating property' + ' email',
+        async () => {
+            const response = await UserModel.update(
+                { ...UserData, email: 'mero2@eampample.com' },
+                user.id
+            );
+            expect(response).toEqual({ ...user, email: 'mero2@eampample.com' });
+        }
+    );
+    it('expects to the response to be empty', async () => {
+        await UserModel.remove(user.id);
+        const response = await UserModel.select(user.id);
+        expect(response).toBeFalsy();
     });
 });
